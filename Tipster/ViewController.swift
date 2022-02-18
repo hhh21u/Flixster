8a495fb0630f856261f599609627d96ed9c1053c
hhh21u <73049911+hhh21u@users.noreply.github.com>
2022-02-18 11:10:37 -0500

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Chen Hanrui on 2022/2/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
